4cdd5e4a2c170f78904d416791f8eca2ddc308ad
Aryagorjipour <arygorji.pour@gmail.com>
2021-09-12 18:42:06 +0430

    initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class TicTacToe implements ActionListener{
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel botton_panel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean playerOne_turn;
+
+    TicTacToe(){
+        
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
